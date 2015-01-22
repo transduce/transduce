056507c6c5d6a5d717826e972282e3625ecd06d3
Kevin Beaty <kevin@simplectic.com>
2015-01-21 18:39:35 -0600

    Move test/tests into test/base

--- test/base.js
+++ test/base.js
@@ -1,30 +1,148 @@
 'use strict'
+var tr = require('../'),
+    test = require('tape'),
+    compose = tr.compose
 
-var test = require('tape');
-var tr = require('../');
+function isOdd(x){return x % 2 === 1}
+function not(p){
+  return function(v){
+    return !p(v)
+  }
+}
+var isEven = not(isOdd)
+function identity(v){return v}
 
 var stringReduce = {
   init: function() {
-    return '';
+    return ''
   },
   step: function(result, val) {
     return result + val
   },
   result: function(result) {
-    return result;
+    return result
   }
 }
 
 test('transduce', function(t) {
-  t.equal(tr.transduce(tr.unique.dedupe(), stringReduce, '', ['a', 'b', 'b', 'c']), 'abc');
-  t.equal(tr.transduce(tr.unique.dedupe(), stringReduce, ['a', 'b', 'b', 'c']), 'abc');
+  t.equal(tr.transduce(tr.unique.dedupe(), stringReduce, '', ['a', 'b', 'b', 'c']), 'abc')
+  t.equal(tr.transduce(tr.unique.dedupe(), stringReduce, ['a', 'b', 'b', 'c']), 'abc')
   t.end()
 })
 
 test('reduce', function(t) {
-  var reducer  = tr.unique.dedupe()(stringReduce);
+  var sum = tr.reduce(function(sum, num){ return sum + num }, 0, [1,2,3])
+  t.equal(sum, 6, 'can sum up an array')
 
-  t.equal(tr.reduce(reducer, '', ['a', 'b', 'b', 'c']), 'abc');
-  t.equal(tr.reduce(reducer, ['a', 'b', 'b', 'c']), 'abc');
+  var prod = tr.reduce(function(prod, num){ return prod * num }, 1, [1, 2, 3, 4])
+  t.equal(prod, 24, 'can reduce via multiplication')
+
+  var reducer  = tr.unique.dedupe()(stringReduce)
+  t.equal(tr.reduce(reducer, '', ['a', 'b', 'b', 'c']), 'abc')
+  t.equal(tr.reduce(reducer, ['a', 'b', 'b', 'c']), 'abc')
   t.end()
+})
+
+test('map', function(t){
+  t.plan(3)
+
+  var doubled = tr.map(function(num){ return num * 2 })
+  t.deepEqual([2,4,6], tr.into([], doubled, [1,2,3]), 'can double')
+
+  var tripled = tr.map(function(num){ return num * 3 })
+  t.deepEqual([3,6,9], tr.toArray(tripled, [1,2,3]), 'can triple')
+
+  doubled = compose(
+    tr.map(function(num){ return num * 2 }),
+    tr.map(function(num){ return num * 3 }))
+  t.deepEqual([6,12,18], tr.toArray(doubled, [1,2,3]), 'can double and triple in chain value')
+})
+
+test('filter', function(t) {
+  t.plan(1)
+
+  var evenArray = [1, 2, 3, 4, 5, 6]
+
+  t.deepEqual(tr.toArray(tr.filter(isEven), evenArray), [2, 4, 6])
+})
+
+test('remove', function(t) {
+  t.plan(1)
+
+  var odds = tr.toArray(tr.remove(function(num){ return num % 2 === 0 }), [1, 2, 3, 4, 5, 6])
+  t.deepEqual(odds, [1, 3, 5], 'rejected each even number')
+})
+
+test('take', function(t) {
+  t.plan(5)
+
+  t.deepEqual(tr.toArray(tr.take(0), [1, 2, 3]), [], 'can pass an index to first')
+  t.deepEqual(tr.into([], tr.take(1), [1, 2, 3]), [1], 'can pull out the first element of an array')
+  t.deepEqual(tr.toArray(tr.take(2), [1, 2, 3]), [1, 2], 'can pass an index to first')
+  t.deepEqual(tr.into([], tr.take(3), [1, 2, 3]), [1, 2, 3], 'can pass an index to first')
+  t.strictEqual(tr.toArray(tr.take(-1), [1, 2, 3]).length, 0)
+})
+
+test('takeWhile', function(t) {
+  t.plan(4)
+
+  t.deepEqual(tr.toArray(tr.takeWhile(isOdd), [1, 2, 3]), [1])
+  t.deepEqual(tr.into([], tr.takeWhile(isEven), [1, 2, 3]), [])
+  t.deepEqual(tr.toArray(tr.takeWhile(isEven), [2, 2, 3]), [2,2])
+  t.deepEqual(tr.into([], tr.takeWhile(isOdd), [1, 3, 3]), [1, 3, 3])
+})
+
+test('drop', function(t) {
+  t.plan(4)
+
+  var numbers = [1, 2, 3, 4]
+  t.deepEqual(tr.into([], tr.drop(1), numbers), [2, 3, 4], 'working rest()')
+  t.deepEqual(tr.toArray(tr.drop(0), numbers), [1, 2, 3, 4], 'working rest(0)')
+  t.deepEqual(tr.into([], tr.drop(-1), numbers), [1, 2, 3, 4], 'working rest(-1)')
+  t.deepEqual(tr.toArray(tr.drop(2), numbers), [3, 4], 'rest can take an index')
+})
+
+test('dropWhile', function(t) {
+  t.plan(4)
+
+  t.deepEqual(tr.toArray(tr.dropWhile(isOdd), [1, 2, 3]), [2,3])
+  t.deepEqual(tr.toArray(tr.dropWhile(isEven), [1, 2, 3]), [1,2,3])
+  t.deepEqual(tr.into([], tr.dropWhile(isEven), [2, 2, 3]), [3])
+  t.deepEqual(tr.into([], tr.dropWhile(isOdd), [1, 3, 3]), [])
+})
+
+test('cat', function(t) {
+  t.plan(1)
+  var res = tr.toArray(tr.cat, [[1,2,3],[4,5,6],[7,8,9]])
+  t.deepEqual(res, [1,2,3,4,5,6,7,8,9])
+})
+
+
+test('mapcat', function(t) {
+  t.plan(1)
+  var res = tr.toArray(tr.mapcat(function(arr){return arr.reverse()}), [[3,2,1],[6,5,4],[9,8,7]])
+  t.deepEqual(res, [1,2,3,4,5,6,7,8,9])
+})
+
+test('partitionBy', function(t) {
+  t.plan(3)
+  var result = tr.toArray(tr.partitionBy(isOdd), [0,1,1,3,4,6,8,7,7,8])
+  t.deepEqual(result, [[0], [1,1,3], [4,6,8], [7,7], [8]])
+  var arr = [1,1,1,2,2,3,3,3]
+  result = tr.toArray(compose(tr.partitionBy(identity), tr.take(2)), arr)
+  t.deepEqual(result, [[1,1,1],[2,2]])
+  result = tr.toArray(tr.partitionBy(isOdd), [])
+  t.deepEqual(result, [])
+})
+
+test('partitionAll', function(t) {
+  t.plan(4)
+  var result = tr.toArray(tr.partitionAll(2), [0,1,2,3,4,5,6,7,8,9])
+  t.deepEqual(result, [[0,1],[2,3],[4,5],[6,7],[8,9]])
+  result = tr.into([], tr.partitionAll(2), [0,1,2,3,4,5,6,7,8])
+  t.deepEqual(result, [[0,1],[2,3],[4,5],[6,7],[8]])
+  result = tr.toArray(compose(tr.partitionAll(2), tr.take(2)), [0,1,2,3,4,5,6,7,8,9])
+  t.deepEqual(result, [[0,1],[2,3]])
+  result = tr.toArray(tr.partitionAll(1), [])
+  t.deepEqual(result, [])
 })
